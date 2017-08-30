7e7b9e35bcd9e83edae50bedff9089f9e4ffad61
meelrossi <merossi@itba.edu.ar>
2017-08-30 18:42:44 -0300

    added menu

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void ChangeMenuScene (string scenename) {
+		Application.LoadLevel (scenename);
+	}
+
+}
