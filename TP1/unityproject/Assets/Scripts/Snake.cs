cb1509ba016394dc4d76888fc1f5eed6403c8a59
meelrossi <merossi@itba.edu.ar>
2017-09-05 16:54:15 -0300

    create SnakeManager

--- TP1/unityproject/Assets/Scripts/Snake.cs
+++ TP1/unityproject/Assets/Scripts/Snake.cs
@@ -7,31 +7,14 @@ using System.Linq;
 public class Snake : MonoBehaviour {
 
 	public GameObject tailPrefab;
-	public GameObject foodPrefab;
-
-	public Transform borderTop;
-	public Transform borderBottom;
-	public Transform borderLeft;
-	public Transform borderRight;
-
-	public GameObject gameOverPaner;
-
-	public Text scoreText;
-	int score;
-	bool paused = false;
-
-
 	List<Transform> tail = new List<Transform>();
-
 	Vector2 dir = Vector2.right;
 
-	bool ate, dead = false;
+	bool ate;
 
 	void Start () {
-		score = 0;
-		centerSnake ();
+		ate = false;
 		InvokeRepeating("Move", 0.3f, 0.1f);
-		gameOverPaner.SetActive (false);
 	}
 	
 	void Update () {
@@ -50,23 +33,13 @@ public class Snake : MonoBehaviour {
 		transform.Translate(dir);
 
 		if (ate) {
-			// Load Prefab into the world
 			GameObject g =(GameObject)Instantiate(tailPrefab,
 				v,
 				Quaternion.identity);
-
-			// Keep track of it in our tail list
 			tail.Insert(0, g.transform);
-
-			// Reset the flag
 			ate = false;
-		}
-		// Do we have a Tail?
-		else if (tail.Count > 0) {
-			// Move last Tail Element to where the Head was
+		} else if (tail.Count > 0) {
 			tail.Last().position = v;
-
-			// Add to front of list, remove from the back
 			tail.Insert(0, tail.Last());
 			tail.RemoveAt(tail.Count-1);
 		}
@@ -75,73 +48,18 @@ public class Snake : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D coll) {
 		if (coll.name.StartsWith("FoodPrefab")) {
 			ate = true;
-			UpdateScore ();
-			Destroy(coll.gameObject);
-			AddFood ();
+			SnakeManager.instance.EatFood (coll.gameObject);
 		} else {
-			dead = true;
-			CancelInvoke();
-			gameOverPaner.SetActive (true);
+			SnakeManager.instance.Crush ();
 		}
 	}
 
-	void AddFood () {
-		int x = (int)Random.Range(borderLeft.position.x,
-			borderRight.position.x);
-
-		int y = (int)Random.Range(borderBottom.position.y,
-			borderTop.position.y);
-
-		Instantiate (foodPrefab,
-			new Vector2 (x, y),
-			Quaternion.identity);
-	}
-
-	void UpdateScore () {
-		score++;
-		scoreText.text = "" + score;
-	}
-
-
-	public void PauseHandler() {
-		if (!paused) {
-			paused = true;
-			CancelInvoke ();
-		} else {
-			paused = false;
-			InvokeRepeating("Move", 0.3f, 0.1f);
-		}
-	}
-
-	public void RestartSnake() {
-		score = 0;
-		scoreText.text = "" + score;
-	
+	public void ResetSnake () {
 		foreach (Transform t in tail) {
 			Destroy (t.gameObject);
 		}
 		tail = new List<Transform> ();
-		paused = false;
 		dir = Vector2.right;
 		InvokeRepeating("Move", 0.3f, 0.1f);
-		centerSnake ();
-		gameOverPaner.SetActive (false);
-
 	}
-
-	void centerSnake() {
-		int x = (int) (((borderRight.position.x - borderLeft.position.x) / 2) + borderLeft.position.x);
-		int y = (int) (((borderTop.position.y - borderBottom.position.y) / 2) + borderBottom.position.y);
-
-		transform.position = new Vector2(x, y);
-	}
-
-//	void OnGUI() {
-//		
-//		if(dead) {
-//			GUI.Label(new Rect(0,0,Screen.width, Screen.height),"Game Over");
-//		}
-//
-//	}
-
 }
---
+++ TP1/unityproject/Assets/Scripts/SnakeManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SnakeManager : MonoBehaviour {
+
+	public static SnakeManager instance = null;
+
+	public GameObject foodPrefab;
+	public Snake snake;
+
+	// Borders
+	public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+	// Score
+	public Text scoreText;
+	int score;
+
+	// Pause
+	public GameObject pausePanel;
+	public GameObject pauseButton;
+	public Sprite pauseSprite;
+	public Sprite playSprite;
+	private Button btn;
+	bool paused;
+
+	// Game over
+	public GameObject gameOverPanel;
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	void Start () {
+		paused = false;
+		btn = pauseButton.GetComponent<Button> ();
+		pausePanel.SetActive (false);
+
+		gameOverPanel.SetActive (false);
+	
+		score = 0;
+	}
+
+	public void EatFood (GameObject food) {
+		UpdateScore ();
+		Destroy(food);
+		AddFood ();
+	}
+
+	public void Crush () {
+		snake.CancelInvoke();
+		gameOverPanel.SetActive (true);
+	}
+
+	void AddFood () {
+		int x = (int) Random.Range(borderLeft.position.x, borderRight.position.x);
+
+		int y = (int) Random.Range(borderBottom.position.y, borderTop.position.y);
+
+		Instantiate (foodPrefab, new Vector2 (x, y), Quaternion.identity);
+	}
+
+	void UpdateScore () {
+		score++;
+		scoreText.text = "" + score;
+	}
+
+	public void PauseGame () {
+		paused = !paused;
+		Debug.Log (pausePanel);
+		if (paused) {
+			btn.image.overrideSprite = playSprite;
+			snake.CancelInvoke ();
+			pausePanel.SetActive (true);
+		} else {
+			btn.image.overrideSprite = pauseSprite;
+			snake.InvokeRepeating("Move", 0.3f, 0.1f);
+			pausePanel.SetActive (false);
+		}
+	}
+
+	public void RestartSnake() {
+		score = 0;
+		scoreText.text = "" + score;
+	
+		centerSnake ();
+		snake.ResetSnake ();
+
+		gameOverPanel.SetActive (false);
+
+		paused = false;
+		pausePanel.SetActive (false);
+		btn.image.overrideSprite = pauseSprite;
+	}
+
+	void centerSnake() {
+		int x = (int) (((borderRight.position.x - borderLeft.position.x) / 2) + borderLeft.position.x);
+		int y = (int) (((borderTop.position.y - borderBottom.position.y) / 2) + borderBottom.position.y);
+		snake.transform.position = new Vector2(x, y);
+	}
+}
